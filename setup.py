a4348c453025313fdfb11b87b4f9790fcf1785ca
romit <11757603+romitjain@users.noreply.github.com>
2024-08-04 23:55:57 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+Setup file for audiotoken
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="audiotoken",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A package for creating audio tokens",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Romit Jain",
+    author_email="romit@merakilabs.com",
+    url="https://github.com/cmeraki/audiotoken",
+    install_requires=[
+        open("requirements.txt").read().splitlines(),
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        
+        "console_scripts": [
+            "audiotoken=audiotoken.scrc:main",
+        ],
+    },
+)
