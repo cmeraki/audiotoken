1649997bebee1894b169944b49c3ea4dbc07a0fe
romit <11757603+romitjain@users.noreply.github.com>
2024-08-05 18:01:45 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="audiotoken",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     description="A package for creating audio tokens",
     long_description=open("README.md").read(),
